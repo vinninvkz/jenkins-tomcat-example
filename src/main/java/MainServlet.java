a1fba6c9e306a052a5ee434f9b7c80e27be808e3
Vlad <vladzh0610@gmail.com>
2024-09-15 18:59:44 +0300

    first commit

---
+++ src/main/java/MainServlet.java
@@ -0,0 +1,29 @@
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class MainServlet extends HttpServlet {
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+
+    }
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.service(req, resp);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.getWriter().write("Hello\n");
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
